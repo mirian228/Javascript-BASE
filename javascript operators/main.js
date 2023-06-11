5e9869970b98de78161d5a8f45835a6c453a0d2f
mirian228 <pariksini@mail.ru>
2023-06-11 20:33:56 +0400

    Arithmetic operators

---
+++ javascript operators/main.js
@@ -0,0 +1,9 @@
+let x=10, y=20;
+
+// Arithmetic operators + - / % * **
+console.log(x+y); // 30
+console.log(x-y); // -10
+console.log(x/y); // 0.5
+console.log(x%y); // 10
+console.log(x*y); // 200
+console.log(x**y); // 100000000000000000000
