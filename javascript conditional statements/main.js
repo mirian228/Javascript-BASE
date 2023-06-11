686f21fe9ae73e05651e5b77651715f297e405a0
mirian228 <pariksini@mail.ru>
2023-06-11 21:14:09 +0400

    If/Ifelse statements

---
+++ javascript conditional statements/main.js
@@ -0,0 +1,10 @@
+// There are three conditional statements in Java if, else if, Switch case
+ let person_age=15;
+
+ if(person_age>=18) {
+    console.log("person eligible for Vote");
+ } else {
+    console.log("person not eligible for Vote");
+ }
+
+ console.log("Program exited......");
