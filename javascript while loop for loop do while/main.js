da063c27715a1a627ae0cf57eda5aafe36865e87
mirian228 <pariksini@mail.ru>
2023-06-12 11:54:50 +0400

    for loop, while loop, do while loop, break, continue

---
+++ javascript while loop for loop do while/main.js
@@ -0,0 +1,46 @@
+// let i=1;
+
+// while(i<=10) {
+//     console.log(i);
+//     i++;
+// }
+
+/* let i = 2;
+
+while(i<=10) {
+    console.log(i);
+    i+=2;
+} */
+
+// let i = 10;
+
+// while(i>=1) {
+//     console.log(i);
+//     i--;
+// }
+
+// do while
+console.log("do while:")
+let i = 10;
+let b =5;
+
+do {
+console.log(b);
+ b++;
+} while(b<=i);
+
+// for loop
+for(i; i<=20; i++) {
+    console.log(i);
+}
+
+// break, continue
+
+for(let z=1; z<=10; z++) 
+{
+    if(z==5) {
+        break;
+    }
+    console.log(z);
+  
+}
